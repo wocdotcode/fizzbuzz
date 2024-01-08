753a85258507249141348b10270933dbf0f76b25
Wendell Centeno <dellortiz06@gmail.com>
2024-01-09 01:12:31 +0800

    Initial commit

---
+++ FizzBuzz/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using FizzBuzzMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FizzBuzzMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FBuzz()
+        {
+            FizzBuzz model = new();
+
+            model.FizzValue = 3;
+            model.BuzzValue = 5;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FBuzz(FizzBuzz fizzbuzz)
+        {
+            List<string> fbItems = new();
+
+            bool fizz;
+            bool buzz;
+
+            for (int i= 1; i <= 100; i++)
+            {
+                fizz = (i % fizzbuzz.FizzValue == 0);
+                buzz = (i % fizzbuzz.BuzzValue == 0);
+
+                if(fizz == true && buzz == true)
+                {
+                    fbItems.Add("FizzBuzz");
+                } 
+              
+                else if(fizz == true)
+              
+                {
+                    fbItems.Add("Fizz");
+              
+                } else if(buzz == true)
+               
+                {
+                    fbItems.Add("Buzz");
+                }
+                else
+                {
+                    fbItems.Add(i.ToString());
+                }
+            }
+
+            fizzbuzz.Result = fbItems;
+
+            return View(fizzbuzz);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FizzBuzz/Models/FizzBuzz.cs
@@ -0,0 +1,11 @@
+﻿namespace FizzBuzzMVC.Models
+{
+    public class FizzBuzz
+    {
+        public int FizzValue { get; set; }
+
+        public int BuzzValue { get; set; }
+
+        public List<string> Result { get; set; } = new();
+    }
+}
